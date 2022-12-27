89df531773dd09773aacc549edee154eb5681f4a
Optimism Name Service (ONS) <121508920+optimismname@users.noreply.github.com>
2022-12-28 00:28:30 +0600

    Update TestReverseRegistrar.js

--- test/TestReverseRegistrar.js
+++ test/TestReverseRegistrar.js
@@ -1,23 +1,23 @@
 const DummyResolver = artifacts.require('./mocks/DummyResolver.sol');
 const ReverseRegistrar = artifacts.require('ReverseRegistrar.sol');
-const ENS = artifacts.require('ENSRegistry.sol');
+const ONS = artifacts.require('ONSRegistry.sol');
 
-const namehash = require('eth-ens-namehash');
+const namehash = require('op-ons-namehash');
 const sha3 = require('web3-utils').sha3;
 
 contract('ReverseRegistar', function (accounts) {
 
     let node;
-    let registrar, resolver, ens;
+    let registrar, resolver, ons;
 
     beforeEach(async () => {
         node = namehash.hash(accounts[0].slice(2).toLowerCase() + ".addr.reverse");
-        ens = await ENS.new();
+        ons = await ONS.new();
         resolver = await DummyResolver.new();
-        registrar = await ReverseRegistrar.new(ens.address, resolver.address);
+        registrar = await ReverseRegistrar.new(ons.address, resolver.address);
 
-        await ens.setSubnodeOwner('0x0', sha3('reverse'), accounts[0], {from: accounts[0]});
-        await ens.setSubnodeOwner(namehash.hash('reverse'), sha3('addr'), registrar.address, {from: accounts[0]});
+        await ons.setSubnodeOwner('0x0', sha3('reverse'), accounts[0], {from: accounts[0]});
+        await ons.setSubnodeOwner(namehash.hash('reverse'), sha3('addr'), registrar.address, {from: accounts[0]});
     });
 
     it('should calculate node hash correctly', async () => {
@@ -26,26 +26,26 @@ contract('ReverseRegistar', function (accounts) {
 
     it('allows an account to claim its address', async () => {
         await registrar.claim(accounts[1], {from: accounts[0]});
-        assert.equal(await ens.owner(node), accounts[1]);
+        assert.equal(await ons.owner(node), accounts[1]);
     });
 
     it('allows an account to specify resolver', async () =>  {
         await registrar.claimWithResolver(accounts[1], accounts[2], {from: accounts[0]});
-        assert.equal(await ens.owner(node), accounts[1]);
-        assert.equal(await ens.resolver(node), accounts[2]);
+        assert.equal(await ons.owner(node), accounts[1]);
+        assert.equal(await ons.resolver(node), accounts[2]);
     });
 
     it('does not overwrite resolver if not specified', async () => {
         await registrar.claimWithResolver(accounts[1], accounts[2], {from: accounts[0]});
         await registrar.claim(accounts[3], {from: accounts[0]});
 
-        assert.equal(await ens.owner(node), accounts[3]);
-        assert.equal(await ens.resolver(node), accounts[2]);
+        assert.equal(await ons.owner(node), accounts[3]);
+        assert.equal(await ons.resolver(node), accounts[2]);
     });
 
     it('sets name records', async () => {
         await registrar.setName('testname', {from: accounts[0], gas: 1000000});
-        assert.equal(await ens.resolver(node), resolver.address);
+        assert.equal(await ons.resolver(node), resolver.address);
         assert.equal(await resolver.name(node), 'testname');
     });
 
