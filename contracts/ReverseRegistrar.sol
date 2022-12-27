0b4cec167481dd5ee1ac330ba528962d50267554
Optimism Name Service (ONS) <121508920+optimismname@users.noreply.github.com>
2022-12-28 01:10:00 +0600

    Update ReverseRegistrar.sol

--- contracts/ReverseRegistrar.sol
+++ contracts/ReverseRegistrar.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.7.0;
 
-import "./ENS.sol";
+import "./ONS.sol";
 
 abstract contract NameResolver {
     function setName(bytes32 node, string memory name) public virtual;
@@ -10,71 +10,71 @@ contract ReverseRegistrar {
     // namehash('addr.reverse')
     bytes32 public constant ADDR_REVERSE_NODE = 0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2;
 
-    ENS public ens;
+    ONS public ens;
     NameResolver public defaultResolver;
 
     /**
      * @dev Constructor
-     * @param ensAddr The address of the ENS registry.
+     * @param ensAddr The address of the ONS registry.
      * @param resolverAddr The address of the default reverse resolver.
      */
-    constructor(ENS ensAddr, NameResolver resolverAddr) public {
-        ens = ensAddr;
+    constructor(ONS onsAddr, NameResolver resolverAddr) public {
+        ons = onsAddr;
         defaultResolver = resolverAddr;
 
         // Assign ownership of the reverse record to our deployer
-        ReverseRegistrar oldRegistrar = ReverseRegistrar(ens.owner(ADDR_REVERSE_NODE));
+        ReverseRegistrar oldRegistrar = ReverseRegistrar(ons.owner(ADDR_REVERSE_NODE));
         if (address(oldRegistrar) != address(0x0)) {
             oldRegistrar.claim(msg.sender);
         }
     }
 
     /**
-     * @dev Transfers ownership of the reverse ENS record associated with the
+     * @dev Transfers ownership of the reverse ONS record associated with the
      *      calling account.
-     * @param owner The address to set as the owner of the reverse record in ENS.
-     * @return The ENS node hash of the reverse record.
+     * @param owner The address to set as the owner of the reverse record in ONS.
+     * @return The ONS node hash of the reverse record.
      */
     function claim(address owner) public returns (bytes32) {
         return claimWithResolver(owner, address(0x0));
     }
 
     /**
-     * @dev Transfers ownership of the reverse ENS record associated with the
+     * @dev Transfers ownership of the reverse ONS record associated with the
      *      calling account.
-     * @param owner The address to set as the owner of the reverse record in ENS.
+     * @param owner The address to set as the owner of the reverse record in ONS.
      * @param resolver The address of the resolver to set; 0 to leave unchanged.
-     * @return The ENS node hash of the reverse record.
+     * @return The ONS node hash of the reverse record.
      */
     function claimWithResolver(address owner, address resolver) public returns (bytes32) {
         bytes32 label = sha3HexAddress(msg.sender);
         bytes32 node = keccak256(abi.encodePacked(ADDR_REVERSE_NODE, label));
-        address currentOwner = ens.owner(node);
+        address currentOwner = ons.owner(node);
 
         // Update the resolver if required
-        if (resolver != address(0x0) && resolver != ens.resolver(node)) {
+        if (resolver != address(0x0) && resolver != ons.resolver(node)) {
             // Transfer the name to us first if it's not already
             if (currentOwner != address(this)) {
-                ens.setSubnodeOwner(ADDR_REVERSE_NODE, label, address(this));
+                ons.setSubnodeOwner(ADDR_REVERSE_NODE, label, address(this));
                 currentOwner = address(this);
             }
-            ens.setResolver(node, resolver);
+            ons.setResolver(node, resolver);
         }
 
         // Update the owner if required
         if (currentOwner != owner) {
-            ens.setSubnodeOwner(ADDR_REVERSE_NODE, label, owner);
+            ons.setSubnodeOwner(ADDR_REVERSE_NODE, label, owner);
         }
 
         return node;
     }
 
     /**
-     * @dev Sets the `name()` record for the reverse ENS record associated with
+     * @dev Sets the `name()` record for the reverse ONS record associated with
      * the calling account. First updates the resolver to the default reverse
      * resolver if necessary.
      * @param name The name to set for this address.
-     * @return The ENS node hash of the reverse record.
+     * @return The ONS node hash of the reverse record.
      */
     function setName(string memory name) public returns (bytes32) {
         bytes32 node = claimWithResolver(address(this), address(defaultResolver));
@@ -85,7 +85,7 @@ contract ReverseRegistrar {
     /**
      * @dev Returns the node hash for a given account's reverse records.
      * @param addr The address to hash
-     * @return The ENS node hash.
+     * @return The ONS node hash.
      */
     function node(address addr) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(ADDR_REVERSE_NODE, sha3HexAddress(addr)));
@@ -93,7 +93,7 @@ contract ReverseRegistrar {
 
     /**
      * @dev An optimised function to compute the sha3 of the lower-case
-     *      hexadecimal representation of an Ethereum address.
+     *      hexadecimal representation of an Optimism address.
      * @param addr The address to hash
      * @return ret The SHA3 hash of the lower-case hexadecimal encoding of the
      *         input address.
