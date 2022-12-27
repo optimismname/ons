c6f51cdbb180f3fbdf5539a112ee1838792cf493
Optimism Name Service (ONS) <121508920+optimismname@users.noreply.github.com>
2022-12-28 00:22:00 +0600

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-const ENS = artifacts.require("./ENSRegistry.sol");
+const ONS = artifacts.require("./ONSRegistry.sol");
 const FIFSRegistrar = artifacts.require('./FIFSRegistrar.sol');
 
 // Currently the parameter('./ContractName') is only used to imply
@@ -6,7 +6,7 @@ const FIFSRegistrar = artifacts.require('./FIFSRegistrar.sol');
 // not existed, it's valid to put it here.
 // TODO: align the contract name with the source code file name.
 const web3 = new (require('web3'))();
-const namehash = require('eth-ens-namehash');
+const namehash = require('op-ons-namehash');
 
 /**
  * Calculate root node hashes given the top level domain(tld)
@@ -21,7 +21,7 @@ function getRootNodeFromTLD(tld) {
 }
 
 /**
- * Deploy the ENS and FIFSRegistrar
+ * Deploy the ONS and FIFSRegistrar
  *
  * @param {Object} deployer truffle deployer helper
  * @param {string} tld tld which the FIFS registrar takes charge of
@@ -29,20 +29,20 @@ function getRootNodeFromTLD(tld) {
 function deployFIFSRegistrar(deployer, tld) {
   var rootNode = getRootNodeFromTLD(tld);
 
-  // Deploy the ENS first
-  deployer.deploy(ENS)
+  // Deploy the ONS first
+  deployer.deploy(ONS)
     .then(() => {
-      // Deploy the FIFSRegistrar and bind it with ENS
-      return deployer.deploy(FIFSRegistrar, ENS.address, rootNode.namehash);
+      // Deploy the FIFSRegistrar and bind it with ONS
+      return deployer.deploy(FIFSRegistrar, ONS.address, rootNode.namehash);
     })
     .then(function() {
       // Transfer the owner of the `rootNode` to the FIFSRegistrar
-      return ENS.at(ENS.address).then((c) => c.setSubnodeOwner('0x0', rootNode.sha3, FIFSRegistrar.address));
+      return ONS.at(ONS.address).then((c) => c.setSubnodeOwner('0x0', rootNode.sha3, FIFSRegistrar.address));
     });
 }
 
 module.exports = function(deployer, network) {
-  var tld = 'eth';
+  var tld = 'op';
 
   if (network === 'dev.fifs') {
     deployFIFSRegistrar(deployer, tld);
