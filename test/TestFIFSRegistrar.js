0598312e080d106033991c0193c525d869b7a278
Optimism Name Service (ONS) <121508920+optimismname@users.noreply.github.com>
2022-12-28 00:30:34 +0600

    Update TestFIFSRegistrar.js

--- test/TestFIFSRegistrar.js
+++ test/TestFIFSRegistrar.js
@@ -1,40 +1,40 @@
 const FIFSRegistrar = artifacts.require('FIFSRegistrar.sol');
-const ENS = artifacts.require('ENSRegistry.sol');
+const ONS = artifacts.require('ONSRegistry.sol');
 
-const { exceptions } = require('@ensdomains/test-utils');
+const { exceptions } = require('@optimismname/test-utils');
 const sha3 = require('web3-utils').sha3;
-const namehash = require('eth-ens-namehash');
+const namehash = require('op-ons-namehash');
 
 contract('FIFSRegistrar', function (accounts) {
 
-    let registrar, ens;
+    let registrar, ons;
 
     beforeEach(async () => {
-        ens = await ENS.new();
-        registrar = await FIFSRegistrar.new(ens.address, '0x0');
+        ons = await ONS.new();
+        registrar = await FIFSRegistrar.new(ons.address, '0x0');
 
-        await ens.setOwner('0x0', registrar.address, {from: accounts[0]})
+        await ons.setOwner('0x0', registrar.address, {from: accounts[0]})
     });
 
     it('should allow registration of names', async () => {
-        await registrar.register(sha3('eth'), accounts[0], {from: accounts[0]});
-        assert.equal(await ens.owner('0x0'), registrar.address);
-        assert.equal(await ens.owner(namehash.hash('eth')), accounts[0]);
+        await registrar.register(sha3('op'), accounts[0], {from: accounts[0]});
+        assert.equal(await ons.owner('0x0'), registrar.address);
+        assert.equal(await ons.owner(namehash.hash('op')), accounts[0]);
     });
 
     describe('transferring names', async () => {
 
         beforeEach(async () => {
-            await registrar.register(sha3('eth'), accounts[0], {from: accounts[0]});
+            await registrar.register(sha3('op'), accounts[0], {from: accounts[0]});
         });
 
         it('should allow transferring name to your own', async () => {
-            await registrar.register(sha3('eth'), accounts[1], {from: accounts[0]});
-            assert.equal(await ens.owner(namehash.hash('eth')), accounts[1]);
+            await registrar.register(sha3('op'), accounts[1], {from: accounts[0]});
+            assert.equal(await ons.owner(namehash.hash('op')), accounts[1]);
         });
 
         it('forbids transferring the name you do not own', async () => {
-            await exceptions.expectFailure(registrar.register(sha3('eth'), accounts[1], {from: accounts[1]}));
+            await exceptions.expectFailure(registrar.register(sha3('op'), accounts[1], {from: accounts[1]}));
         });
     });
 });
