40165bf09986837adc2b3c78054880656b1570e0
Optimism Name Service (ONS) <121508920+optimismname@users.noreply.github.com>
2022-12-28 00:38:58 +0600

    Update and rename TestENS.js to TestONS.js

--- test/TestONS.js
+++ test/TestONS.js
@@ -1,27 +1,27 @@
-const namehash = require('eth-ens-namehash');
+const namehash = require('op-ons-namehash');
 const sha3 = require('web3-utils').sha3;
 
-const { exceptions } = require("@ensdomains/test-utils")
+const { exceptions } = require("@optimismname/test-utils")
 
 let contracts = [
-    [artifacts.require('ENSRegistry.sol'), 'Solidity']
+    [artifacts.require('ONSRegistry.sol'), 'Solidity']
 ];
 
-contracts.forEach(function ([ENS, lang]) {
-    contract('ENS ' + lang, function (accounts) {
+contracts.forEach(function ([ONS, lang]) {
+    contract('ONS ' + lang, function (accounts) {
 
-        let ens;
+        let ons;
 
         beforeEach(async () => {
-            ens = await ENS.new();
+            ons = await ONS.new();
         });
 
         it('should allow ownership transfers', async () => {
             let addr = '0x0000000000000000000000000000000000001234';
 
-            let result = await ens.setOwner('0x0', addr, {from: accounts[0]});
+            let result = await ons.setOwner('0x0', addr, {from: accounts[0]});
 
-            assert.equal(await ens.owner('0x0'), addr)
+            assert.equal(await ons.owner('0x0'), addr)
 
             assert.equal(result.logs.length, 1);
             let args = result.logs[0].args;
@@ -31,16 +31,16 @@ contracts.forEach(function ([ENS, lang]) {
 
         it('should prohibit transfers by non-owners', async () => {
             await exceptions.expectFailure(
-                ens.setOwner('0x1', '0x0000000000000000000000000000000000001234', {from: accounts[0]})
+                ons.setOwner('0x1', '0x0000000000000000000000000000000000001234', {from: accounts[0]})
             );
         });
 
         it('should allow setting resolvers', async () => {
             let addr = '0x0000000000000000000000000000000000001234'
 
-            let result = await ens.setResolver('0x0', addr, {from: accounts[0]});
+            let result = await ons.setResolver('0x0', addr, {from: accounts[0]});
 
-            assert.equal(await ens.resolver('0x0'), addr);
+            assert.equal(await ons.resolver('0x0'), addr);
 
             assert.equal(result.logs.length, 1);
             let args = result.logs[0].args;
@@ -50,14 +50,14 @@ contracts.forEach(function ([ENS, lang]) {
 
         it('should prevent setting resolvers by non-owners', async () => {
             await exceptions.expectFailure(
-                ens.setResolver('0x1', '0x0000000000000000000000000000000000001234', {from: accounts[0]})
+                ons.setResolver('0x1', '0x0000000000000000000000000000000000001234', {from: accounts[0]})
             );
         });
 
         it('should allow setting the TTL', async () => {
-            let result = await ens.setTTL('0x0', 3600, {from: accounts[0]});
+            let result = await ons.setTTL('0x0', 3600, {from: accounts[0]});
 
-            assert.equal((await ens.ttl('0x0')).toNumber(), 3600);
+            assert.equal((await ons.ttl('0x0')).toNumber(), 3600);
 
             assert.equal(result.logs.length, 1);
             let args = result.logs[0].args;
@@ -66,23 +66,23 @@ contracts.forEach(function ([ENS, lang]) {
         });
 
         it('should prevent setting the TTL by non-owners', async () => {
-            await exceptions.expectFailure(ens.setTTL('0x1', 3600, {from: accounts[0]}));
+            await exceptions.expectFailure(ons.setTTL('0x1', 3600, {from: accounts[0]}));
         });
 
         it('should allow the creation of subnodes', async () => {
-            let result = await ens.setSubnodeOwner('0x0', sha3('eth'), accounts[1], {from: accounts[0]});
+            let result = await ons.setSubnodeOwner('0x0', sha3('op'), accounts[1], {from: accounts[0]});
 
-            assert.equal(await ens.owner(namehash.hash('eth')), accounts[1]);
+            assert.equal(await ons.owner(namehash.hash('op')), accounts[1]);
 
             assert.equal(result.logs.length, 1);
             let args = result.logs[0].args;
             assert.equal(args.node, "0x0000000000000000000000000000000000000000000000000000000000000000");
-            assert.equal(args.label, sha3('eth'));
+            assert.equal(args.label, sha3('op'));
             assert.equal(args.owner, accounts[1]);
         });
 
         it('should prohibit subnode creation by non-owners', async () => {
-            await exceptions.expectFailure(ens.setSubnodeOwner('0x0', sha3('eth'), accounts[1], {from: accounts[1]}));
+            await exceptions.expectFailure(ons.setSubnodeOwner('0x0', sha3('op'), accounts[1], {from: accounts[1]}));
         });
     });
 });
